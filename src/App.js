caddf1d144e16158381c5b04651fa3a72c833091
Breno Godoy <godoyy25z@gmail.com>
2022-09-27 02:23:58 -0300

    feat: adiciona créditos aos ícones usados na página

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Footer from './Footer';
 import Page from './Page';
 
 function App() {
   return (
     <div className="App">
       <Page/>
+      <Footer/>
     </div>
   );
 }
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 function Footer() {
   return (
-    <div>footer</div>
+    <div>
+      <p>ícones retirados de:
+        <a href='https://www.flaticon.com/br/'>https://www.flaticon.com/br/</a>
+      </p>
+    </div>
   )
 }
 
